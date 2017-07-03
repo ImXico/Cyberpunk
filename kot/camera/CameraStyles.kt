5c006de7de73a34a70b91f22c5d99e0077609d7b
ImXico <fjm.cunha@campus.fct.unl.pt>
2017-07-03 18:24:28 +0100

    Added explicit types where they were missing

--- kot/camera/CameraStyles.kt
+++ kot/camera/CameraStyles.kt
@@ -11,7 +11,7 @@ import com.badlogic.gdx.math.Vector3
  * @param worldHeight height of the world (world coordinates).
  */
 fun Camera.center(worldWidth: Int, worldHeight: Int) {
-  val newPosition = Vector3(worldWidth / 2f, worldHeight / 2f, 0f)
+  val newPosition: Vector3 = Vector3(worldWidth / 2f, worldHeight / 2f, 0f)
   this.refresh(newPosition)
 }
 
@@ -22,7 +22,7 @@ fun Camera.center(worldWidth: Int, worldHeight: Int) {
  * @param targetPosition position of the target, where the camera will be also positioned to.
  */
 fun Camera.lockOn(targetPosition: Vector2) {
-  val newPosition = Vector3(targetPosition.x, targetPosition.y, 0f)
+  val newPosition: Vector3 = Vector3(targetPosition.x, targetPosition.y, 0f)
   this.refresh(newPosition)
 }
 
@@ -34,7 +34,7 @@ fun Camera.lockOn(targetPosition: Vector2) {
  * @param yOffset camera's Y offset, relatively to the [targetPosition] Y.
  */
 fun Camera.lockOnX(targetPosition: Vector2, yOffset: Float) {
-  val newPosition = Vector3(targetPosition.x, targetPosition.y + yOffset, 0f)
+  val newPosition: Vector3 = Vector3(targetPosition.x, targetPosition.y + yOffset, 0f)
   this.refresh(newPosition)
 }
 
@@ -46,7 +46,7 @@ fun Camera.lockOnX(targetPosition: Vector2, yOffset: Float) {
  * @param xOffset camera's X offset, relatively to the [targetPosition] X.
  */
 fun Camera.lockOnY(targetPosition: Vector2, xOffset: Float) {
-  val newPosition = Vector3(targetPosition.x + xOffset, targetPosition.y, 0f)
+  val newPosition: Vector3 = Vector3(targetPosition.x + xOffset, targetPosition.y, 0f)
   this.refresh(newPosition)
 }
 
@@ -62,9 +62,9 @@ fun Camera.lockOnY(targetPosition: Vector2, xOffset: Float) {
  */
 @JvmOverloads
 fun Camera.lerpTo(targetPosition: Vector2, lerp: Float, xOffset: Float = 0f, yOffset: Float = 0f) {
-  val newX = this.position.x + ((targetPosition.x - position.x) * lerp) + xOffset
-  val newY = this.position.y + ((targetPosition.y - position.y) * lerp) + yOffset
-  val newPosition = Vector3(newX, newY, 0f)
+  val newX: Float = this.position.x + ((targetPosition.x - position.x) * lerp) + xOffset
+  val newY: Float = this.position.y + ((targetPosition.y - position.y) * lerp) + yOffset
+  val newPosition: Vector3 = Vector3(newX, newY, 0f)
   this.refresh(newPosition)
 }
 
