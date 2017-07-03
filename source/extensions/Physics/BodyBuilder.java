b493d397e3768e53ff8f63d636f2945b9324de9a
ImXico <fjm.cunha@campus.fct.unl.pt>
2017-07-03 11:36:48 +0100

    Added missing import

--- source/extensions/Physics/BodyBuilder.java
+++ source/extensions/Physics/BodyBuilder.java
@@ -2,6 +2,7 @@ package source.extensions.Physics;
 
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.utils.Array;
 
