365660d4f857ecbc6cdbc336b4ea97eb6afb317e
ImXico <fjm.cunha@campus.fct.unl.pt>
2017-07-11 20:00:01 +0100

    Changed some styling issues for the PhysicsConfig

--- kot/physics/PhysicsConfig.kt
+++ kot/physics/PhysicsConfig.kt
@@ -3,25 +3,25 @@ package kot.physics
 import com.badlogic.gdx.math.Vector2
 
 /**
- * Default value for the [World] gravity.
+ * Default value for the [PhysicsWorld.world] gravity.
  */
 private val DEFAULT_GRAVITY: Vector2 = Vector2(0f, -9.8f)
 
 /**
  * The amount of time to simulate, this should not vary.
- * @see World#step
+ * @see PhysicsWorld.world#step
  */
 private val DEFAULT_TIMESTEP: Float = 1 / 60f
 
 /**
  * For the velocity constraint solver.
- * @see World#step
+ * @see PhysicsWorld.world#step
  */
 private val DEFAULT_VELOCITY_ITERATIONS: Int = 6
 
 /**
  * For the position constraint solver.
- * @see World#step
+ * @see PhysicsWorld.world#step
  */
 private val DEFAULT_POSITION_ITERATIONS: Int = 2
 
@@ -31,9 +31,10 @@ private val DEFAULT_POSITION_ITERATIONS: Int = 2
  * as seen above.
  * Once created, the attributes of a [PhysicsConfig] cannot be changed.
  */
-data class PhysicsConfig(
-        val gravity: Vector2 = DEFAULT_GRAVITY,
-        val timestep: Float = DEFAULT_TIMESTEP,
-        val velocityIterations: Int = DEFAULT_VELOCITY_ITERATIONS,
-        val positionIterations: Int = DEFAULT_POSITION_ITERATIONS
+data class PhysicsConfig
+(
+  val gravity: Vector2 = DEFAULT_GRAVITY,
+  val timestep: Float = DEFAULT_TIMESTEP,
+  val velocityIterations: Int = DEFAULT_VELOCITY_ITERATIONS,
+  val positionIterations: Int = DEFAULT_POSITION_ITERATIONS
 )
